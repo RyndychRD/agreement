165b5bf65b16fa97b745767ed95ad9216d36ca6e
RyndychRD <r.roman1234@yandex.ru>
2022-12-28 16:01:12 +0600

    Маленький фикс отображения

--- client/src/components/pages/adminSettings/catalogs/departments/buttonModals/update.jsx
+++ client/src/components/pages/adminSettings/catalogs/departments/buttonModals/update.jsx
@@ -5,13 +5,14 @@ import {
 import { AUseForm } from "../../../../../adapter";
 import ModalUpdate from "../../../../../fragments/modals/modalUpdate";
 import CreateUpdateForm from "./createUpdateForm";
+import getUniqNotNullIds from "../../../../../../services/CommonFunctions";
 
 export default function UpdateButtonModel() {
   // Служит для отслеживания формы из модального окна для обработки по кнопке
   const [form] = AUseForm();
   const formDefaultValues = (data) => ({
     newDepartmentName: data?.name,
-    rightIds: data?.rights?.map((el) => el.id),
+    rightIds: getUniqNotNullIds(data?.rights),
   });
   return (
     <ModalUpdate
