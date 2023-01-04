f800ea6d34bcd3ceabcf2932186e6b1f91429a1b
RyndychRD <r.roman1234@yandex.ru>
2023-01-04 09:17:40 +0600

    Добавил миграцию и сидирование по имени права

--- srv/db/seeds/seeds/rights.js
+++ srv/db/seeds/seeds/rights.js
@@ -7,10 +7,14 @@ exports.rightsSeed = async function (knex) {
   await knex("rights").del();
   await knex.raw("SELECT setval('rights_id_seq', 4, true);");
   await knex("rights").insert([
-    { id: "1", name: "Администратор" },
-    { id: "2", name: "Просмотр согласования договоров" },
-    { id: "3", name: "Доступ к удалению" },
-    { id: "4", name: "Доступ к созданию" },
+    { id: "1", name: "Администратор", code_name: "Admin" },
+    {
+      id: "2",
+      name: "Просмотр согласования договоров",
+      code_name: "contractAgreement",
+    },
+    { id: "3", name: "Доступ к удалению", code_name: "Delete" },
+    { id: "4", name: "Доступ к созданию", code_name: "Create" },
   ]);
   console.log("rightsSeed executed");
 };
