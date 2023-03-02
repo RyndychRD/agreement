fbb7ba4a67d23f7e55a3dabaf705cc826b12a288
RyndychRD <r.roman1234@yandex.ru>
2023-03-02 14:22:06 +0600

    Удалил пока не реализованный функционал

--- client/src/components/fragments/documentControl/documentToArchive/documentToArchiveFragment.jsx
+++ client/src/components/fragments/documentControl/documentToArchive/documentToArchiveFragment.jsx
@@ -1,34 +1,3 @@
-import { HeaderTextOutput } from "../../outputs/textOutputs";
-import { useGetDocumentQueryHook } from "../../../../core/redux/api/DocumentControl/DocumentApi";
-import SimpleSpinner from "../../messages/Spinner";
-import SimpleError from "../../messages/Error";
-import DocumentToArchiveShow from "./documentToArchiveShow";
-
-export default function DocumentToArchiveFragment(props) {
+export default function DocumentToArchiveFragment() {
   return "";
-  const { documentId, closeModalFunc } = props;
-  const {
-    data: document = {},
-    isLoading,
-    isError,
-  } = useGetDocumentQueryHook({
-    id: documentId,
-    // Пока что не реализовано
-    isGetDocumentArchiveType: true,
-  });
-  return (
-    <>
-      <HeaderTextOutput text="Перемещение в архив" />
-      {isLoading ? <SimpleSpinner /> : ""}
-      {isError ? <SimpleError /> : ""}
-      {!isLoading && !isError ? (
-        <DocumentToArchiveShow
-          document={document}
-          closeMainModal={closeModalFunc}
-        />
-      ) : (
-        ""
-      )}
-    </>
-  );
 }
