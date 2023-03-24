71fc0d458a76c2ed249965b69af0e0d00c8f83a8
RyndychRD <r.roman1234@yandex.ru>
2023-03-24 13:54:45 +0600

    Добавил миграции для типов поручений

---
+++ srv/db/migrations/20230324061405_create_document_task_types.js
@@ -0,0 +1,43 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("document_task_types", function (table) {
+      table.increments("id");
+      table.string("name").comment("Название типа задачи");
+      table
+        .string("uniq_name")
+        .unique()
+        .comment("Название типа задачи для использования в разработке");
+      table.comment("Таблица со списком всех возможных типов задач");
+    })
+    .then(() => {
+      return knex("document_task_types").insert([
+        { id: 1, name: "Обычное поручение", uniq_name: "default" },
+      ]);
+    })
+    .then(() => {
+      return knex.schema.alterTable("document_tasks", function (table) {
+        table
+          .integer("document_task_type_id")
+          .comment("Ссылка на тип поручения")
+          .defaultTo(1)
+          .references("document_task_types.id")
+          .onDelete("SET NULL");
+      });
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .alterTable("document_tasks", function (table) {
+      table.dropColumn("document_task_type_id");
+    })
+    .dropTableIfExists("document_task_types");
+};
---
+++ srv/db/seeds/seeds/document_task_types.js
@@ -0,0 +1,26 @@
+const { seedTable } = require("../../seedHelper/seedHelper");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.documentTaskTypesSeed = async function (knex) {
+  const arr = [
+    {
+      id: 2,
+      name: "Поручение для 2 листа согласования Закупа ТРУ",
+      uniq_name: "getInformationForSecondPageZakupTRU",
+    },
+  ];
+
+  const table = "document_task_types";
+
+  await seedTable(knex, {
+    table: table,
+    arr: arr,
+    isIgnoreConflict: true,
+    index: arr.length,
+  });
+
+  console.log("documentTaskTypesSeed executed");
+};
