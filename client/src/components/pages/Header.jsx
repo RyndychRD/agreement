e789b7d206fe307b6daff4b65044ff9b26e40f32
RyndychRD <r.roman1234@yandex.ru>
2023-04-12 09:42:02 +0600

    До переключения

--- client/src/components/pages/Header.jsx
+++ client/src/components/pages/Header.jsx
@@ -9,8 +9,6 @@ import {
   userNameMask,
 } from "../../services/CommonFunctions";
 import { AMenu, ASpan, AArrowLeftOutlined, APageHeader } from "../adapter";
-import WSS from "../../core/socket/WebSocket";
-
 /**
  * Главный хедер, отображается на всех страницах, кроме авторизации
  * К хедеру привязана логика проверки авторизован ли пользователь
@@ -25,8 +23,6 @@ function Header() {
   useEffect(() => {
     console.log("isAuth: ", isAuth);
     if (!isAuth) {
-      // Если авторизация не прошла, то мы должны закрыть существующее соединение
-      WSS.close();
       navigate("/login", {
         state: {
           prev_location: `${location.pathname}?${location.search}`.replace(
@@ -35,10 +31,6 @@ function Header() {
           ),
         },
       });
-    } else {
-      // Если у нас есть авторизация, то мы должны создать новое подключение при необходимости
-      // eslint-disable-next-line no-unused-vars
-      const wss = new WSS();
     }
   }, [isAuth, location.pathname, location.search, navigate]);
 
@@ -60,12 +52,12 @@ function Header() {
           label: "Справка",
           key: "FAQ",
         },
-        // isAccessGranted("Admin")
-        //   ? {
-        //       label: "Справка (Админ)",
-        //       key: "FAQ_admin",
-        //     }
-        //   : "",
+        isAccessGranted("Admin")
+          ? {
+              label: "Справка (Админ)",
+              key: "FAQ_admin",
+            }
+          : "",
         // {
         //   label: "Аккаунт",
         //   key: "account",
