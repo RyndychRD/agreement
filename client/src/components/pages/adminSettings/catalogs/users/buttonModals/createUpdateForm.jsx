6cd80f5fcabd0faf9f4c34f5bb71eae796eec04a
RyndychRD <r.roman1234@yandex.ru>
2023-04-12 16:16:17 +0600

    Сделал валидацию на уникальный логин

--- client/src/components/pages/adminSettings/catalogs/users/buttonModals/createUpdateForm.jsx
+++ client/src/components/pages/adminSettings/catalogs/users/buttonModals/createUpdateForm.jsx
@@ -9,18 +9,35 @@ import {
 import CheckboxInputFormItem from "../../../../../fragments/inputs/checkboxInputs";
 import { useGetRightsQueryHook } from "../../../../../../core/redux/api/Globals/Catalogs/RightApi";
 import getUniqNotNullIds from "../../../../../../services/CommonFunctions";
+import { useGetUsersQueryHook } from "../../../../../../core/redux/api/Globals/Catalogs/UserApi";
 
-export default function CreateUpdateForm({ form, isAddUpdateOnlyFields }) {
+export default function CreateUpdateForm({
+  form,
+  rawData,
+  isAddUpdateOnlyFields,
+}) {
   const {
     data: positions = {},
     isLoading,
     isError,
   } = useGetPositionsQueryHook({});
+
+  const {
+    data: users = {},
+    isLoadingUsers,
+    isErrorUsers,
+  } = useGetUsersQueryHook({});
+
   const {
     data: rights = {},
     isError: isErrorRights,
     isLoading: isLoadingRights,
   } = useGetRightsQueryHook();
+
+  let userLogins = [];
+  if (!isLoadingUsers && !isErrorUsers && users.length > 0) {
+    userLogins = users.map((user) => user.login);
+  }
 
   const checkbox = isAddUpdateOnlyFields ? (
     <CheckboxInputFormItem title="Заблокирован?" name="isDisabled" />
@@ -61,6 +78,19 @@ export default function CreateUpdateForm({ form, isAddUpdateOnlyFields }) {
             required: true,
             message: "Введите логин пользователя",
           },
+          () => ({
+            validator(_, value) {
+              const trimValue = value.trim();
+              // Если это не тот логин, который уже был и он встречается среди других логинов, выводим ошибку
+              if (
+                trimValue !== rawData?.login &&
+                userLogins.find((login) => login === trimValue)
+              ) {
+                return Promise.reject(new Error("Такой логин занят"));
+              }
+              return Promise.resolve();
+            },
+          }),
         ]}
       />
       <TextInputFormItem
