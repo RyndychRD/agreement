74d41a68805a086f4611183da5a154de9e672f6d
RyndychRD <r.roman1234@yandex.ru>
2023-04-17 15:00:54 +0600

    Добавил поддержку проверки авторизации для получения файлов

--- srv/src/router/file-router.js
+++ srv/src/router/file-router.js
@@ -2,10 +2,11 @@ const Router = require("express").Router;
 const router = new Router();
 const DocumentFileController = require("../controllers/file-controller");
 const fileAuthMiddleware = require("../middlewares/file-auth-middleware");
+const authMiddleware = require("../middlewares/auth-middleware");
 const rightMiddleware = require("../middlewares/right-middleware");
 
 // prettier-ignore
 router.post("/",fileAuthMiddleware,rightMiddleware(), DocumentFileController.uploadFile);
-router.get("/", DocumentFileController.getFile);
+router.get("/", authMiddleware, DocumentFileController.getFile);
 
 module.exports = router;
